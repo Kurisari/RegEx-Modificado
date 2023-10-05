f815aca9d23786602eac23d59797cc4539e07f90
Cristian Aragon <crisarag8.sal@gmail.com>
2023-10-05 17:09:11 -0600

    Primera idea: diccionario

---
+++ config.py
@@ -0,0 +1,16 @@
+config = {
+    "find": {
+        "px": "f",
+    },
+    "find_replace": {
+        "px": "fr",
+    },
+    "global": {
+        "global_flag": "g",
+        "insensitive_flag": "i",
+        "appear": "?",
+        "any_letter": "*",
+        "or": "|",
+    }
+    
+}
