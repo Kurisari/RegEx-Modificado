0ec833592daa3c2007efaf8083cb573306f01537
Cristian Aragon <crisarag8.sal@gmail.com>
2023-10-05 22:38:21 -0600

    Prueba de implementación find

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from config import config
+from commands import Find
 
 prefix_find = config["find"]["px"]
 prefix_find_replace = config["find_replace"]["px"]
+
+prueba = Find.Find("algorithm", "texto.txt")
+print(prueba.search())
