59752c6558c4f10295f8ca7984e3dc6ab8f2b7ca
Cristian Aragon <crisarag8.sal@gmail.com>
2023-10-06 13:35:27 -0600

    Creación de palabras con rango

--- commands/find.py
+++ commands/find.py
@@ -3,14 +3,17 @@ class Find:
     def __init__(self, filename):
         self.ALPHABET_SIZE = config["global"]["ALPHABET_SIZE"]
         self.filename = filename
+    
     def __char_to_index(self, char):
         return ord(char)
+    
     def __calculate_bad_match_table(self, pattern):
         p = len(pattern)
         bad = [p]*self.ALPHABET_SIZE
         for k in range(p - 1):
             bad[self.__char_to_index(pattern[k])] = p - k - 1
         return bad
+    
     def search(self, pattern):
         if pattern == "":
             return
@@ -34,13 +37,22 @@ class Find:
                         matches.append((line, text_idx - patt_size + 1))
                     text_idx += bad_match_table[ord(current_line[min(text_idx, len(current_line) - 1)])]  # Modificación aquí
                 current_line = file.readline()
+        file.close()
         return matches
+    
     def __end_bracket(self, pattern, bracket_idx):
         for i in range(bracket_idx, len(pattern)-1):
             curr = self.__char_to_index(pattern[i])
             if curr == 93:
                 return i
         return
+    
     def range_search(self, pattern, bracket_idx):
         end_bracket_idx = self.__end_bracket(pattern, bracket_idx)
-        print(bracket_idx, end_bracket_idx)
+        first_part = pattern[:bracket_idx]
+        range_part = pattern[bracket_idx + 1: end_bracket_idx]
+        second_part = pattern[end_bracket_idx+1:]
+        words = []
+        for i in range(len(range_part)):
+            words.append(first_part + range_part[i] + second_part)
+        return words
