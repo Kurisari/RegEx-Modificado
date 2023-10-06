1d689fa5d77ee8a05d084c292faa592c6d74995a
Cristian Aragon <crisarag8.sal@gmail.com>
2023-10-06 14:24:36 -0600

    Prueba de busqueda por rango

--- src/main.py
+++ src/main.py
@@ -6,4 +6,4 @@ prefix_find_replace = config["find_replace"]["px"]
 
 prueba = Find.Find("texto.txt")
 # print(prueba.search("algorithms"))
-print(prueba.set_search("al[agc]orithms", 2))
+print(prueba.range_search("al[a-t]orithms", 2))
