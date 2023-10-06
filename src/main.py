1727fabf608ad56416c15783b41cd999a72da6eb
Cristian Aragon <crisarag8.sal@gmail.com>
2023-10-06 15:49:24 -0600

    Prueba búsqueda con llaves

--- src/main.py
+++ src/main.py
@@ -7,4 +7,6 @@ prefix_find_replace = config["find_replace"]["px"]
 prueba = Find.Find("texto.txt")
 # print(prueba.search("algorithms"))
 # print(prueba.range_search("al[a-t]orithms", 2))
-print(prueba.all_char_search("alg*rithms"))
+# print(prueba.all_char_search("alg*rithms"))
+# print(prueba.or_search("algorithms | algorithm"))
+print(prueba.key_search("algor{5}ithm", 5))
