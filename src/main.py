415f975bbdd25a46deed86f52e742e6602e0e5ea
Cristian Aragon <crisarag8.sal@gmail.com>
2023-10-06 15:05:26 -0600

    Prueba de función asterisco

--- src/main.py
+++ src/main.py
@@ -6,4 +6,5 @@ prefix_find_replace = config["find_replace"]["px"]
 
 prueba = Find.Find("texto.txt")
 # print(prueba.search("algorithms"))
-print(prueba.range_search("al[a-t]orithms", 2))
+# print(prueba.range_search("al[a-t]orithms", 2))
+print(prueba.all_char_search("alg*rithms"))
