755c8262c4f65d6bd716d04c3a78a70a1ee7c5dc
Cristian Aragon <crisarag8.sal@gmail.com>
2023-10-10 13:10:39 -0600

    Eliminacion comentarios residuales

--- src/main.py
+++ src/main.py
@@ -6,15 +6,6 @@ from events import Identifier
 prefix_find = config["find"]["px"]
 prefix_find_replace = config["find_replace"]["px"]
 
-# prueba = Find.Find("texto.txt")
-# print(prueba.search("algorithms"))
-# print(prueba.range_search("al[a-t]orithms", 2))
-# print(prueba.all_char_search("alg*rithms"))
-# print(prueba.or_search("algorithms | algorithm"))
-# print(prueba.key_search("algor{5}ithm", 5))
-# print(prueba.question_search("algor?ithms"))
-
-
 user_input = ""
 while user_input != " ":
     user_input = input()
