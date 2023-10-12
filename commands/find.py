0dbf02944461482e77f266b7b08768fa03f8f83a
Cristian Aragon <crisarag8.sal@gmail.com>
2023-10-12 13:43:51 -0600

    Error solucionado de rango

--- commands/find.py
+++ commands/find.py
@@ -15,11 +15,11 @@ class Find:
         return bad
     
     def __symbol_finder(self, pattern, start, ascii):
-        for i in range(start, len(pattern)-1):
+        for i in range(start, len(pattern)):
             curr = self.__char_to_index(pattern[i])
             if curr == ascii:
                 return i
-        return
+        return i
     
     def search(self, pattern, flag1, flag2):
         if pattern == "":
